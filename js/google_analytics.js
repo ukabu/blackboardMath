d7c2476c123fc07a9ee4e2cca986de1700ab2b59
Ukabu <emmanuel@pirsch.org>
2011-02-18 14:18:19 -0500

    Add analytics to head instead of body

--- js/google_analytics.js
+++ js/google_analytics.js
@@ -7,7 +7,7 @@ Analytics = {
     setTimeout(function() {
       var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
       ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-      var s = document.body.appendChild(ga);
+      var s = document.getElementsByTagName("head")[0].appendChild(ga);
     }, 500);
   }
 };
