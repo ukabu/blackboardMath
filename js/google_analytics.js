fe13f24db9a45c68fc0a1ca4297716f55cdf9158
Ukabu <emmanuel@pirsch.org>
2011-02-20 14:25:06 -0500

    Increase delay before loading analytics

--- js/google_analytics.js
+++ js/google_analytics.js
@@ -8,6 +8,6 @@ Analytics = {
       var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
       ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
       var s = document.getElementsByTagName("head")[0].appendChild(ga);
-    }, 500);
+    }, 2000);
   }
 };
